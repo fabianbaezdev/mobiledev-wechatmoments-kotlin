e8bbeb00b9866d108dd3415a0a2e8e6043993324
neox <neoxleonhart@gmail.com>
2024-03-09 21:06:01 -0300

    Refactor: Cleaning old View classes

--- app/src/main/java/com/tws/moments/TWApplication.kt
+++ app/src/main/java/com/tws/moments/TWApplication.kt
@@ -1,26 +1,18 @@
 package com.tws.moments
 
 import android.app.Application
-import com.tws.moments.ui.imageloader.GlideImageLoader
-import com.tws.moments.ui.imageloader.ImageLoader
 import com.tws.moments.utils.ScreenAdaptiveUtil
 
 class TWApplication : Application() {
 
     companion object {
         lateinit var app: Application
-        lateinit var imageLoader: ImageLoader
     }
 
     override fun onCreate() {
         super.onCreate()
         app = this
         ScreenAdaptiveUtil.adaptive(this)
-
-        initImageLoader()
     }
 
-    private fun initImageLoader() {
-        imageLoader = GlideImageLoader(this)
-    }
 }
